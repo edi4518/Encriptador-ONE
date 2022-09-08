96565db15b5222ba8c74f6f00c38206a6e9a0c61
Eduardo Incrocci <edi4518@gmail.com>
2022-09-08 12:05:23 -0300

    correcciones en el modelo responsivo

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const panelDcho = document.querySelector(".divpaneldcho");
 const panelDchoRes = document.querySelector(".divpaneldchoresultado");
 
 function encriptar(stringEncriptado){
+    
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"], ["u", "ufat"]]
     stringEncriptado = stringEncriptado.toLowerCase();
 
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptado.includes(matrizCodigo[i][0])) {
-         stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }     
-    }
-    return stringEncriptado;
 
-}
+    for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptado.includes(matrizCodigo[i][0])) {
+    stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+            }     
+        }   
+    
+    return stringEncriptado;
+    }
+   
+
 
 
 
@@ -22,29 +26,38 @@ function desencriptar(stringDesencriptado){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"], ["u", "ufat"]]
     stringDesencriptado = stringDesencriptado.toLowerCase();
 
+   
     for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
-            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-        }     
+    if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+    stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+            }     
     }
     return stringDesencriptado;
-
+   
 }
 
 function btnEncriptar(){
-    const mensajeEncriptado = encriptar(inputTexto.value);
-    mensaje.value = mensajeEncriptado;
-    panelDcho.style.display = "none";
-    panelDchoRes.style.display = "block";
-    inputTexto.value = "";
+    
+    if(inputTexto.value !=""){
+        const mensajeEncriptado = encriptar(inputTexto.value);
+        mensaje.value = mensajeEncriptado;
+        panelDcho.style.display = "none";
+        panelDchoRes.style.display = "block";
+        inputTexto.value = "";
+    }
+    else{
+        alert("Por favor ingrese texto a encriptar")
+    }
 }
 
 function btnDesencriptar(){
+    if(inputTexto.value !=""){
     const mensajeEncriptado = desencriptar(inputTexto.value);
     mensaje.value = mensajeEncriptado;
     panelDcho.style.display = "none";
     panelDchoRes.style.display = "block";
     inputTexto.value = "";
+    }
 }
 
 
@@ -55,3 +68,5 @@ function copiarTexto(){
     alert("Texto Copiado", 'success');
 
 }
+
+
